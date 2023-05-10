e1205be24f2af07897f92ef829311bea661eaf08
david Hernandez <davidhhv3@gmail.com>
2023-05-10 01:52:54 -0500

    add db and getData

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Data;
 using SocialMedia.Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
@@ -30,7 +32,11 @@ namespace SocialMedia.Api
             services.AddControllers();
 
             //
-            services.AddTransient<IPostRepository, PostMongoRepository>();
+            services.AddDbContext<SocialMediaContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("SocialMedia"))
+            );
+            services.AddTransient<IPostRepository, PostRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
