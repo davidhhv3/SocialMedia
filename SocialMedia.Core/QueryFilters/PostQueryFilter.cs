a2839a87e8e4c4d832dcd43b8269a668631add4b
david Hernandez <davidhhv3@gmail.com>
2023-05-16 18:08:41 -0500

    add pagination

--- SocialMedia.Core/QueryFilters/PostQueryFilter.cs
+++ SocialMedia.Core/QueryFilters/PostQueryFilter.cs
@@ -10,8 +10,8 @@ namespace SocialMedia.Core.QueryFilters
 
         public string Description { get; set; }
 
-        //public int PageSize { get; set; }
+        public int PageSize { get; set; }
 
-        //public int PageNumber { get; set; }
+        public int PageNumber { get; set; }
     }
 }
